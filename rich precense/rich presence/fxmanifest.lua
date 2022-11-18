d7105eabe7279310f72aec21d60bb83f069df289
W!lleDev <116746549+W1lleDev@users.noreply.github.com>
2022-11-18 01:28:52 +0100

    Add files via upload

---
+++ rich precense/rich presence/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+-- http://discord.gg/vBDKxs6kjn -- 
+
+author 'W1lle#3121 from c8 Services'
+description 'Simple discord rich precense made by W1lle#3121 from c8 Services'
+version '1.0.0'
+
+
+client_scripts {
+    'client.lua'
+}
